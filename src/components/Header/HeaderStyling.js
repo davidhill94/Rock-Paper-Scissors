8fba3eeb4ce6219725728a0acef719c202e7fd0b
David <david94hill@gmail.com>
2023-05-10 10:43:42 +0100

    Color change

--- src/components/Header/HeaderStyling.js
+++ src/components/Header/HeaderStyling.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const HeaderWrapper = styled.div`
 width: 100%;
 height: 15vh;
-border: 2px solid #000;
+border: 2px solid var(--theme-tertiary);
 display: grid;
 grid-template-columns: 50% 50%;
 place-items: center;
@@ -21,7 +21,7 @@ justify-content: center;
 `
 export const AreaTwo = styled.div`
 grid-column: 2 / 3;
-background-color: white;
+background-color: var(--theme-secondary);
 display: flex;
 height: 100%;
 width: 100%;
